3ad32fbbe2b92f9e6b8c7df1bed7f2df5fe49ac0
GiggleLiu <cacate0129@gmail.com>
2025-02-25 16:45:02 +0800

    new yao test

--- test/test_yaoext.jl
+++ test/test_yaoext.jl
@@ -10,3 +10,22 @@ using Yao, PauliPropagation, Test
     @test circ == circuit
     @test thetas == thetas
 end
+
+@testset "simulation" begin
+    nq = 16
+    pstr = PauliString(nq, :Z, 12)
+    nl = 4
+    topology = bricklayertopology(nq; periodic=false)
+
+    circuit = hardwareefficientcircuit(nq, nl; topology=topology)
+    nparams = countparameters(circuit)
+    thetas = randn(nparams) * 0.5;
+
+    yao_circ = yao_circuit(nq, circuit, thetas)
+    psum_exact = propagate(circuit, pstr, thetas; min_abs_coeff=0)
+    psum_yao = apply!(zero_state(nq), yao_circ)
+
+    exact_expectation = overlapwithzero(psum_exact)
+    yao_expectation = expect(put(nq, 12 => Z), psum_yao)
+    @test isapprox(exact_expectation, yao_expectation, atol=1e-6)
+end
